cbe4f13a7d4fbfd3846ae9d4a366a38380bb88ab
Wayaer <wayaer@foxmail.com>
2020-10-26 15:09:09 +0800

    no message

--- ios/AppTempRNTests/AppTempRNTests.m
+++ ios/AppTempRNTests/AppTempRNTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface app_temp_rnTests : XCTestCase
+@interface AppTempRNTests : XCTestCase
 
 @end
 
-@implementation app_temp_rnTests
+@implementation AppTempRNTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
